a89d981ede9ef232e360979eecd539c1ae24d7a5
Fadil <fadilfdl2710@gmail.com>
2025-05-24 07:53:57 +0700

    save

---
+++ index.js
@@ -0,0 +1,11 @@
+// NOTE: UNCOMMENT FOR TESTING CODE
+
+// NOTE: Method 1
+// const learn = require("./learn.js");
+// console.log(learn.greeting("Nabila", "Teacher"));
+// console.log(learn.calculator(2, 2));
+
+// NOTE: Method 2
+// const { name, employee, Jobdesk, greeting, calculator } = require("./yourfile");
+// console.log(greeting("Nabila", "Teacher"));
+// console.log(calculator(2, 2));
--- learn.js
+++ learn.js
@@ -1,14 +1,53 @@
+// NOTE: Function
 const greeting = (name, work) => {
   return `Hello, my name is ${name} and Iam a ${work}.`;
 };
 
 const calculator = (value1, value2) => {
-  return value1 + value2;
+  const result = value1 + value2;
+  return `${value1} + ${value2} = ${result}`;
 };
 
-console.log(greeting("Nabila", "Teacher"));
-console.log(calculator(5, 7));
+// NOTE: variable
+const name = "John";
 
-// NOTE: export
-module.exports = greeting;
-module.exports = calculator;
+// NOTE: object
+const employee = {
+  name: "Shofa",
+  old: 20,
+  printEmployee() {
+    return `Name: ${this.name}, Age: ${this.old}`;
+  },
+};
+
+// NOTE: class
+class Jobdesk {
+  constructor() {
+    console.log("Jobdesk Class telah dibuat dan di inisialisasi");
+  }
+}
+
+// NOTE: export 1
+// module.exports.name = name;
+// module.exports.employee = employee;
+// module.exports.Jobdesk = Jobdesk;
+// module.exports.greeting = greeting;
+// module.exports.calculator = calculator;
+
+// NOTE: export 2
+// module.exports = {
+//   name: name,
+//   employee: employee,
+//   Jobdesk: Jobdesk,
+//   greeting: greeting,
+//   calculator: calculator,
+// };
+
+// NOTE: export 3 (if same name)
+module.exports = {
+  name,
+  employee,
+  Jobdesk,
+  greeting,
+  calculator,
+};
