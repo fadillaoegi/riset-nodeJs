14fd76af2ab224b196b675367ab4beb975536d40
Fadil <fadilfdl2710@gmail.com>
2025-05-26 15:42:30 +0700

    contact Apps Npm

---
+++ contactAppsNpm/app.js
@@ -0,0 +1,11 @@
+// get arguments from command line
+
+// NOTE: import
+const command = process.argv[2];
+const yargs = require("yargs");
+
+// NOTE: initial
+
+// NOTE: execute
+// console.log(process.argv);
+// console.log(yargs.argv);
---
+++ contactAppsNpm/contacts.js
@@ -0,0 +1,49 @@
+const rl = require("readline");
+const fs = require("fs");
+
+const input = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const question = (x) => {
+  return new Promise((resolve, reject) => {
+    input.question(`Enter your ${x}: `, (x) => {
+      resolve(x.trim());
+    });
+  });
+};
+
+const saveContact = (name, age, email) => {
+  const fileName = "dataUser";
+  const dirPath = "./data";
+  const filePath = `${dirPath}/${fileName}.json`;
+
+  // NOTE: create directori
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
+
+  // NOTE: create file
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, JSON.stringify({ users: [] }));
+    // fs.writeFileSync(filePath, "[]", "utf8");
+  }
+
+  // NOTE: write data from input to fileJson
+  const dataUser = [];
+  const dataBuffer = fs.readFileSync("data/dataUser.json", "utf8");
+  const dataJson = JSON.parse(dataBuffer);
+  dataJson.users.push({
+    name: name.trim(),
+    age: parseInt(age.trim(), 10),
+    email: email.trim(),
+  });
+  fs.writeFileSync(filePath, JSON.stringify(dataJson, null, 2));
+  input.close();
+};
+
+module.exports = {
+  question,
+  saveContact,
+};
