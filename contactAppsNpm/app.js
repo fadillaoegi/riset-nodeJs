df5fcc1916f125aadabb221112307234d4943d98
fadillaoegi <fadilfdl2710@gmail.com>
2025-05-29 17:04:46 +0700

    update

--- contactAppsNpm/app.js
+++ contactAppsNpm/app.js
@@ -1,33 +1,49 @@
-// get arguments from command line
-
 // NOTE: import
-const command = process.argv[2];
-const yargs = require("yargs");
+// const command = process.argv[2];
+const yargs = require("yargs")
+// const {saveContact} = require("./contacts")
 
 // NOTE: add data
-yargs.command(
-  "add",
-  "Create new data user",
-  () => {},
-  (argv) => {
-    console.log(argv.name);
-  }
-);
+yargs
+  .command({
+    command: "add",
+    describe: "Add new contact",
+    builder: {
+      name: {
+        describe: "Contact name",
+        demandOption: true,
+        type: "string",
+      },
+      email: {
+        describe: "Contact email",
+        demandOption: true,
+        type: "string",
+      },
+      phone: {
+        describe: "Contact phone",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler: (argv) => {
+      const newContact = {
+        name: argv.name,
+        email: argv.email,
+        phone: argv.phone,
+      }
+      console.log(newContact)
+      // saveContact(newContact);
+    },
+  })
+  .demandCommand()
 
 // NOTE: List data
-yargs.command(
-  "list",
-  "List data",
-  () => {},
-  (argv) => {
-    console.log(argv.name);
-  }
-);
+
 // NOTE: List detail data
 // NOTE: Delete data
 
 // NOTE: execute
-yargs.parse();
+yargs.parse()
 
 // console.log(process.argv);
 // console.log(yargs.argv);
--- contactAppsNpm/contacts.js
+++ contactAppsNpm/contacts.js
@@ -1,40 +1,39 @@
-const rl = require("readline");
-const fs = require("fs");
-const { saveContact } = require("./contacts");
+// const rl = require("readline");
+const fs = require("fs")
 
 const input = rl.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
 const question = (x) => {
   return new Promise((resolve, reject) => {
     input.question(`Enter your ${x}: `, (x) => {
-      resolve(x.trim());
-    });
-  });
-};
+      resolve(x.trim())
+    })
+  })
+}
 
 const saveContact = (name, age, email) => {
-  const fileName = "dataUser";
-  const dirPath = "./data";
-  const filePath = `${dirPath}/${fileName}.json`;
+  const fileName = "dataUser"
+  const dirPath = "./data"
+  const filePath = `${dirPath}/${fileName}.json`
 
   // NOTE: create directori
   if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
+    fs.mkdirSync(dirPath)
   }
 
   // NOTE: create file
   if (!fs.existsSync(filePath)) {
-    fs.writeFileSync(filePath, JSON.stringify({ users: [] }));
+    fs.writeFileSync(filePath, JSON.stringify({users: []}))
     // fs.writeFileSync(filePath, "[]", "utf8");
   }
 
   // NOTE: write data from input to fileJson
-  const dataUser = [];
-  const dataBuffer = fs.readFileSync("data/dataUser.json", "utf8");
-  const dataJson = JSON.parse(dataBuffer);
+  const dataUser = []
+  const dataBuffer = fs.readFileSync("data/dataUser.json", "utf8")
+  const dataJson = JSON.parse(dataBuffer)
 
   // NOTE: Checking if data already exist
   // const is
@@ -43,12 +42,12 @@ const saveContact = (name, age, email) => {
     name: name.trim(),
     age: parseInt(age.trim(), 10),
     email: email.trim(),
-  });
-  fs.writeFileSync(filePath, JSON.stringify(dataJson, null, 2));
-  input.close();
-};
+  })
+  fs.writeFileSync(filePath, JSON.stringify(dataJson, null, 2))
+  // input.close();
+}
 
 module.exports = {
   question,
   saveContact,
-};
+}
